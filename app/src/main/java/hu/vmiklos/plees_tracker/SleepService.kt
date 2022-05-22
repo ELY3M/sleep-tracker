f7ece7ebeaa551b5fc681827ed09b9e81c4c088c
ELY M <elymbmx@gmail.com>
2022-05-22 14:29:45 -0500

    added licnesce header in SleepService

--- app/src/main/java/hu/vmiklos/plees_tracker/SleepService.kt
+++ app/src/main/java/hu/vmiklos/plees_tracker/SleepService.kt
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2022 ELY M. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+ 
+ 
 package hu.vmiklos.plees_tracker
 
 import android.app.Service
